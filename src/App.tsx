2bc79d69ce317aa8a5822eb1f93f305c4e3829f4
heronet <sirat4757@gmail.com>
2023-08-12 03:27:24 +0600

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import About from "./components/About/About";
-// import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
@@ -14,7 +14,7 @@ function App() {
       <Skills />
       <Projects />
       <About />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,13 @@
+import classes from "./footer.module.scss";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <p>© 2023 Sirat. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
