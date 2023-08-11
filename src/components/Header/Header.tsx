140a2135bad8b7cb3b405dcdf13e4bfae609dce2
heronet <sirat4757@gmail.com>
2023-08-12 04:49:22 +0600

    addeddropdown

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import MenuButton from "../../ui/MenuButton";
 import classes from "./header.module.scss";
+import CloseButton from "../../ui/CloseButton";
 
 const links = [
   { id: 1, title: "Skills", url: "#skills" },
@@ -8,23 +10,37 @@ const links = [
 ];
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={classes.container}>
       <div className={classes["links-container"]}>
         <a href="#" className={classes.brand}>
           Heronet<span className={classes.xyz}>.xyz</span>
         </a>
-        {/* For big screens */}
         <ul className={classes.links}>
           {links.map((link) => (
             <li key={link.id} className={classes.link}>
               <a href={link.url}>{link.title}</a>
             </li>
           ))}
-          <li className={classes.menu}>
-            <MenuButton />
+          <li
+            className={classes.menu}
+            onClick={() => setIsOpen((open) => !open)}
+          >
+            {isOpen ? <CloseButton /> : <MenuButton />}
           </li>
         </ul>
+        {isOpen && (
+          <ul className={classes.dropdown}>
+            {links.map((link) => (
+              <li key={link.id} className={classes.dropitem}>
+                <a href={link.url} onClick={() => setIsOpen((open) => !open)}>
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
---
+++ src/ui/CloseButton.tsx
@@ -0,0 +1,19 @@
+const CloseButton = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      strokeWidth={1.5}
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
+
+export default CloseButton;
